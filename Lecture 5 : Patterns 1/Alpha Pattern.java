abd00ff4fb2a9d7e5b917cb9b1ea0fae56170e8a
Alip Kumar Nayak <129895461+Alipnayak@users.noreply.github.com>
2023-04-07 00:52:46 +0530

    Create Alpha Pattern.java

---
+++ Lecture 5 : Patterns 1/Alpha Pattern.java
@@ -0,0 +1,63 @@
+Alpha Pattern
+Send Feedback
+Print the following pattern for the given N number of rows.
+Pattern for N = 3
+ A
+ BB
+ CCC
+Input format :
+Integer N (Total no. of rows)
+Output format :
+Pattern in N lines
+Constraints
+0 <= N <= 26
+Sample Input 1:
+7
+Sample Output 1:
+A
+BB
+CCC
+DDDD
+EEEEE
+FFFFFF
+GGGGGGG
+Sample Input 2:
+6
+Sample Output 2:
+A
+BB
+CCC
+DDDD
+EEEEE
+FFFFFF
+
+CODE START FROM HERE
+
+import java.util.*;
+public class Solution {
+
+
+	public static void main(String[] args) {
+		
+		/* Your class should be named Solution.
+	 	* Read input as specified in the question.
+	 	* Print output as specified in the question.
+		*/
+        Scanner sc= new Scanner(System.in);
+        int N = sc.nextInt();
+        char c=65;
+        for(int i=1;i<=N;i++)
+        {          
+                     for(int j=0;j<i;j++)
+                    {    
+                        System.out.print(c);
+                       
+                    }  
+         c++;
+        System.out.println("");
+        }
+
+		
+	}
+
+}
